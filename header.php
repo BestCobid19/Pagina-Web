c294a0a49813824a34c92eecbe0e4f366273751f
vzontor <63230088+vzontor@users.noreply.github.com>
2020-04-13 12:09:37 +0200

    m

--- header.php
+++ header.php
@@ -5,26 +5,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Project Coronao</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
 </head>
 
 <body>
-    <nav class="nav bordenavbar">
-        <ul class="nav">
-            <li class="nav-item">
-                <a class="nav-link active" href="#">¿Que es el COVID-19?</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link active" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link active" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link active" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-            </li>
-        </ul>
+    <nav class="nav navbar-expand-lg bordenavbar navbar-light">
+        
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="nav">
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">¿Que es el COVID-19?</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">Sintomas</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">¿Que hacer si estas enfermo?</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="#" tabindex="-1" aria-disabled="true">Evite enfermarse</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">Lo que usted puede hacer</a>
+                </li>
+            </ul>
+        </div>
     </nav>
