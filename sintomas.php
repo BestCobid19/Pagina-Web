885583e21c51d162d967b3f0af988948fd8d0acc
vzontor <63230088+vzontor@users.noreply.github.com>
2020-04-16 02:33:13 +0200

    Nuevo Sintomas

--- sintomas.php
+++ sintomas.php
@@ -5,18 +5,33 @@ include('header.php');
 <div class="jumbotron jumbotron-fluid jumbotron1">
     <div class="container">
         <h1 class="display-4 bordeBot">Coronavirus (COVID-19)</h1>
-        
-        
+
+
     </div>
 </div>
 <div class="container borde">
-        <div class="texto">
-            <h1><span class="bordeIzquierdaFino"></span>¿Cuáles son los síntomas de la COVID-19?</h1>
-            <p>
-            Los síntomas más comunes de la COVID-19 son fiebre, cansancio y tos seca. Algunos pacientes pueden presentar dolores, congestión nasal, rinorrea, dolor de garganta o diarrea. Estos síntomas suelen ser leves y aparecen de forma gradual. Algunas personas se infectan pero no desarrollan ningún síntoma y no se encuentran mal. La mayoría de las personas (alrededor del 80%) se recupera de la enfermedad sin necesidad de realizar ningún tratamiento especial. Alrededor de 1 de cada 6 personas que contraen la COVID-19 desarrolla una enfermedad grave y tiene dificultad para respirar. Las personas mayores y las que padecen afecciones médicas subyacentes, como hipertensión arterial, problemas cardiacos o diabetes, tienen más probabilidades de desarrollar una enfermedad grave. En torno al 2% de las personas que han contraído la enfermedad han muerto. Las personas que tengan fiebre, tos y dificultad para respirar deben buscar atención médica.
-            </p>
+    <div class="texto">
+        <h1><span class="bordeIzquierdaFino"></span>¿Cuáles son los síntomas de la COVID-19?</h1>
+        <p>
+            Los casos confirmados de la enfermedad del coronavirus reportan sintomas que varian de leves a endermedades graves. Estos síntomas pueden aparecer entre 2 a 14 días después de la exposición
+        </p>
+        <ul class="container">
+            <li>Fiebre</li>
+            <li>Tos</li>
+            <li>Dificultad para respirar</li>
+        </ul>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 col-md-offset-5">
+                    <img src="img/fiebre.png" alt="" width="200rem">
+                    <img src="img/tos.png" alt="" width="200rem">
+                    <img src="img/pulmones.png" alt="" srcset="" width="200rem">
+                </div>
+            </div>
         </div>
+
     </div>
+</div>
 <?php
 include('footer.php');
 ?>
