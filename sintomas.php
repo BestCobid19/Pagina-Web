cbe381179f8d7f82bcbbbb4c6754a146fefca6a1
vzontor <63230088+vzontor@users.noreply.github.com>
2020-04-16 02:58:39 +0200

    Udgrade Sintomas

--- sintomas.php
+++ sintomas.php
@@ -4,32 +4,47 @@ include('header.php');
 
 <div class="jumbotron jumbotron-fluid jumbotron1">
     <div class="container">
-        <h1 class="display-4 bordeBot">Coronavirus (COVID-19)</h1>
+        <h1 class="display-4 bordeBot">Síntomas (COVID-19)</h1>
 
 
     </div>
 </div>
 <div class="container borde">
     <div class="texto">
-        <h1><span class="bordeIzquierdaFino"></span>¿Cuáles son los síntomas de la COVID-19?</h1>
+        <h2 class="texto"><span class="bordeIzquierdaFino"></span>¿Cuáles son los síntomas de la COVID-19?</h2>
         <p>
-            Los casos confirmados de la enfermedad del coronavirus reportan sintomas que varian de leves a endermedades graves. Estos síntomas pueden aparecer entre 2 a 14 días después de la exposición
+            Los casos confirmados de la enfermedad del coronavirus reportan sintomas que varian de leves a enfermedades graves. Estos síntomas pueden aparecer entre <strong>2 a 14 días después de la exposición.</strong>
         </p>
         <ul class="container">
             <li>Fiebre</li>
             <li>Tos</li>
             <li>Dificultad para respirar</li>
+            <li>Dolores</li>
+            <li>Congestión nasal</li>
+            <li>Rinorrea</li>
+            <li>Diarrea</li>
         </ul>
         <div class="container">
             <div class="row">
                 <div class="col-md-12 col-md-offset-5">
+                    <p>
+                    Estos síntomas suelen ser leves y aparecen de forma gradual. Algunas personas se infectan pero no desarrollan ningún síntoma y no se encuentran mal. La mayoría de las personas (alrededor del 80%) se recupera de la enfermedad sin necesidad de realizar ningún tratamiento especial. Alrededor de 1 de cada 6 personas que contraen la COVID-19 desarrolla una enfermedad grave y tiene dificultad para respirar.
+                    </p>
                     <img src="img/fiebre.png" alt="" width="200rem">
                     <img src="img/tos.png" alt="" width="200rem">
                     <img src="img/pulmones.png" alt="" srcset="" width="200rem">
                 </div>
             </div>
         </div>
-
+        <h2 class="texto"><span class="bordeIzquierdaFino"></span>¿Cuándo buscar atención médica?</h2>
+        <p>Si manifiesta alguno de estos signos de alarma de emergencia* de COVID-19, <strong>busque atención médica de inmediato:</strong> </p>
+        <ul class="container">
+            <li>Dificultad para respirar</li>
+            <li>Dolor o presión persistente en el pecho</li>
+            <li>Confusión o dificultad para estar alerta que no haya tenido antes</li>
+            <li>Coloración azulada en los labios o el rostro</li>
+        </ul>
+        <p> <strong>Llame al 911 si tiene una emergencia médica:</strong>informe al operador que tiene, o cree que podría tener, COVID-19. De ser posible, colóquese una cubierta de tela para cara antes de que llegue el personal de servicios médicos.</p>
     </div>
 </div>
 <?php
