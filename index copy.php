62e2e36d25217683bb9769be9c30644b1e227051
vzontor <63230088+vzontor@users.noreply.github.com>
2020-04-17 11:23:58 +0200

    NuevoIndex

--- index copy.php
+++ index copy.php
@@ -3,29 +3,29 @@ include('header.php');
 ?>
 <div class="jumbotron jumbotron-fluid jumbotron1">
     <div class="container">
-        <h1 class="display-4 bordeBot">¿Quienes somos?</h1>
-
-
+        <h1 class="display-4 bordeBot">Coronavirus (COVID-19)</h1>
+        
+        
     </div>
 </div>
 
-<div class="container borde">
-    <div class="row justify-content-around marginAll">
-        <div class="col-6">
-            <h1><span class="bordeIzquierdaFino"></span>Luchamos contra la desinformación.</h1>
+    <div class="container borde">
+        <div class="texto">
+            <h1><span class="bordeIzquierdaFino"></span>¿Quiénes somos?</h1>
             <p>Somos dos estudiantes de CFGM que tenemos como objetivo hacer esta página web para el proyecto de FCT con el final de informar a la población
-                sobre la COVID-19, explicando que es, características, consejos y bulos.
-            </p>
+                sobre la COVID-19, explicando que es, características, consejos y bulos. 
+                </p>
+        </div>
+        <div class="texto">
+
+            <h1><span class="bordeIzquierdaFino"></span>Nuestras fuentes</h1>
             <p>Todo la información que se muestra en esta página es información que proviene de fuentes fiables:</p>
             <li> Organización Mundial de la Salud</li> 
             <li> Información del gobierno</li>
             <li> www.thefitmedstudent.com</li>
         </div>
-        <div class="col-4">
-            <img src="img/informaticos3.jpg" alt="" srcset="" width="350rem">
-        </div>
     </div>
-</div>
+
 <?php
 include('footer.php');
 ?>
