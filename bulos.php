88d971a625f0264ba2414610eee73a6173f88d93
AitorPujadas <apujadasm@esliceu.net>
2020-04-20 11:19:53 +0200

    sd

--- bulos.php
+++ bulos.php
@@ -11,16 +11,63 @@ include('header.php');
 
 <div class="container borde">
     <div class="row justify-content-around marginAll">
-        <div class="col-6">
-            <li>El coronavirus COVID-19 fue creado en un laboratorio por humanos. FALSO. El análisis genético publicado
-                en la revista Nature afirma que proviene de la naturaleza.
-            </li>
-            <li>
-                Las redes 5G de telefonía móvil propagan la COVID-19. FALSO. Los virus no se desplazan por las ondas electromagnéticas ni las redes de telefonía móvil. La COVID-19 se está propagando en numerosos países en los que no existe una red 5G.
-            </li>
+        <div class="col-2">
+        <img src="img/laboratorio.png" alt="" srcset="" width="200rem">
         </div>
-        <div class="col-4">
-            <img src="img/informaticos3.jpg" alt="" srcset="" width="350rem">
+        <div class="col-6">
+            <ul>
+                <li>El coronavirus COVID-19 fue creado en un laboratorio por humanos. FALSO. El análisis genético publicado
+                    en la revista Nature afirma que proviene de la naturaleza.
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="row justify-content-around marginAll">
+        <div class="col-2">
+        <img src="img/wifi.png" alt="" srcset="" width="160rem">
+        </div>
+        <div class="col-6">
+            <ul>
+                <li>
+                    Las redes 5G de telefonía móvil propagan la COVID-19. FALSO. Los virus no se desplazan por las ondas electromagnéticas ni las redes de telefonía móvil. La COVID-19 se está propagando en numerosos países en los que no existe una red 5G.
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="row justify-content-around marginAll">
+        <div class="col-2">
+        <img src="img/alcohol.png" alt="" srcset="" width="160rem">
+        </div>
+        <div class="col-6">
+            <ul>
+                <li>
+                    Beber alcohol lo protegerá de la COVID-19. FALSO. El consumo frecuente o excesivo de alcohol puede aumentar el riesgo de sufrir problemas de salud.
+                 </li>
+            </ul>
+        </div>
+    </div>
+    <div class="row justify-content-around marginAll">
+        <div class="col-2">
+        <img src="img/sol.png" alt="" srcset="" width="200rem">
+        </div>
+        <div class="col-6">
+            <ul>
+                 <li>
+                     Exponerse al sol o a temperaturas superiores a los 25 ºC cura la COVID-19. FALSO. Puede contraer la COVID-19, por muy soleado o cálido que sea el clima. Se han notificado casos de COVID-19 en países cálidos. Para protegerse, lávese las manos con frecuencia y a fondo y evite tocarse los ojos, la boca y la nariz.
+                 </li>
+            </ul>
+        </div>
+    </div>
+    <div class="row justify-content-around marginAll">
+        <div class="col-2">
+        <img src="img/bañera.png" alt="" srcset="" width="200rem">
+        </div>
+        <div class="col-6">
+            <ul>
+                 <li>
+                    Bañarse en agua caliente previene la infección por el nuevo coronavirus. FALSO. Bañarse en agua caliente no previene la infeccion por el nuevo coronavirus. 
+                 </li>
+            </ul>
         </div>
     </div>
 </div>
