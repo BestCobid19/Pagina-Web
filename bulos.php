f8e342343cc5c911f61fc29209e96cb2c08cfb71
AitorPujadas <apujadasm@esliceu.net>
2020-04-20 11:31:13 +0200

    Update bulos.php

--- bulos.php
+++ bulos.php
@@ -15,8 +15,9 @@ include('header.php');
         <img src="img/laboratorio.png" alt="" srcset="" width="200rem">
         </div>
         <div class="col-6">
+        <h3>¿De dónde sale el coronavirus?</h3>
             <ul>
-                <li>El coronavirus COVID-19 fue creado en un laboratorio por humanos. FALSO. El análisis genético publicado
+                <li>El coronavirus COVID-19 fue creado en un laboratorio por humanos. <strong>FALSO</strong>. El análisis genético publicado
                     en la revista Nature afirma que proviene de la naturaleza.
                 </li>
             </ul>
@@ -27,9 +28,10 @@ include('header.php');
         <img src="img/wifi.png" alt="" srcset="" width="160rem">
         </div>
         <div class="col-6">
+        <h3>¿Las redes propagan la COVID-19?</h3>
             <ul>
                 <li>
-                    Las redes 5G de telefonía móvil propagan la COVID-19. FALSO. Los virus no se desplazan por las ondas electromagnéticas ni las redes de telefonía móvil. La COVID-19 se está propagando en numerosos países en los que no existe una red 5G.
+                    Las redes 5G de telefonía móvil propagan la COVID-19. <strong>FALSO</strong>. Los virus no se desplazan por las ondas electromagnéticas ni las redes de telefonía móvil. La COVID-19 se está propagando en numerosos países en los que no existe una red 5G.
                 </li>
             </ul>
         </div>
@@ -39,9 +41,10 @@ include('header.php');
         <img src="img/alcohol.png" alt="" srcset="" width="160rem">
         </div>
         <div class="col-6">
+        <h3>¿Beber alcohol me protegerá?</h3>
             <ul>
                 <li>
-                    Beber alcohol lo protegerá de la COVID-19. FALSO. El consumo frecuente o excesivo de alcohol puede aumentar el riesgo de sufrir problemas de salud.
+                    Beber alcohol lo protegerá de la COVID-19. <strong>FALSO</strong>. El consumo frecuente o excesivo de alcohol puede aumentar el riesgo de sufrir problemas de salud.
                  </li>
             </ul>
         </div>
@@ -51,9 +54,10 @@ include('header.php');
         <img src="img/sol.png" alt="" srcset="" width="200rem">
         </div>
         <div class="col-6">
+        <h3>¿Las altas temperaturas me protegerán?</h3>
             <ul>
                  <li>
-                     Exponerse al sol o a temperaturas superiores a los 25 ºC cura la COVID-19. FALSO. Puede contraer la COVID-19, por muy soleado o cálido que sea el clima. Se han notificado casos de COVID-19 en países cálidos. Para protegerse, lávese las manos con frecuencia y a fondo y evite tocarse los ojos, la boca y la nariz.
+                     Exponerse al sol o a temperaturas superiores a los 25 ºC cura la COVID-19. <strong>FALSO</strong>. Puede contraer la COVID-19, por muy soleado o cálido que sea el clima. Se han notificado casos de COVID-19 en países cálidos. Para protegerse, lávese las manos con frecuencia y a fondo y evite tocarse los ojos, la boca y la nariz.
                  </li>
             </ul>
         </div>
@@ -63,9 +67,10 @@ include('header.php');
         <img src="img/bañera.png" alt="" srcset="" width="170rem">
         </div>
         <div class="col-6">
+        <h3>¿Bañarse en agua caliente previene el coronavirus?</h3>
             <ul>
                  <li>
-                    Bañarse en agua caliente previene la infección por el nuevo coronavirus. FALSO. Bañarse en agua caliente no previene la infeccion por el nuevo coronavirus. 
+                    Bañarse en agua caliente previene la infección por el nuevo coronavirus. <strong>FALSO</strong>. Bañarse en agua caliente no previene la infeccion por el nuevo coronavirus. 
                  </li>
             </ul>
         </div>
