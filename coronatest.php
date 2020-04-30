fb8fe7de9e2fdf6501bb647c8ed803f7228dd100
AitorPujadas <apujadasm@esliceu.net>
2020-04-30 10:46:25 +0200

    Update coronatest.php

--- coronatest.php
+++ coronatest.php
@@ -4,7 +4,7 @@ include('header.php');
 <div class="covidtest">
 <h1>Aquí podras hacer un test del COVID-19</h1>
 <div id="test_corona">
-
+    <input type="button" value="Empezar test"></input>
 </div>
 </div>
 
