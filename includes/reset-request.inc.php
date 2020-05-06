bcadf8d6c5bd6a389dbc0703741dcd064d4a87b2
AitorPujadas <apujadasm@esliceu.net>
2020-05-06 15:44:55 +0200

    newpassword

---
+++ includes/reset-request.inc.php
@@ -0,0 +1,57 @@
+<?php
+
+if (isset($_POST["reset-request-submit"])) {
+
+    $selector = bin2hex(random_bytes(8));
+    $token = random_bytes(32);
+
+    $url = "www.reycorona.epizy.com/create-new-password.php?selector=" . $selector . "&validator=" . bin2hex($token);
+
+    $expires = date("U") + 1800;
+
+    require 'dbh.inc.php';
+
+    $userEmail = $_POST["email"];
+
+    $sql = "DELETE FROM pwdReset WHERE pwdResetEmail=?;";
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql)){
+        echo "Eso ha sido un error";
+        exit();
+    } else {
+         mysqli_stmt_bind_param($stmt, "s", $userEmail);
+         mysqli_stmt_execute($stmt);
+    }
+
+    $sql = "INSERT INTO pwdReset (pwdResetEmail, pwdResetSelector, pwdResetToken, pwdResetExpires) VALUES (?, ?, ?, ?);";
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql)){
+        echo "Eso ha sido un error";
+        exit();
+    } else {
+         $hashedToken = password_hash($token, PASSWORD_DEFAULT);
+         mysqli_stmt_bind_param($stmt, "ssss", $userEmail, $selector, $hashedToken, $expires);
+         mysqli_stmt_execute($stmt);
+    }
+
+    mysqli_stmt_close($stmt);
+    mysqli_close($conn);
+
+    $to = $userEmail;
+
+    $subject = 'Renueva tu contraseña para reycorona';
+
+    $message = '<p>Hemos recibido una petición para renovar la contraseña. El link para renovarla es el siguiente.</p>';
+    $message .= '<p>Aquí está tu link para renovar la contraseña: <br>';
+    $message .= '<a href="' . $url . '">' . $url . '</a></p>';
+
+    $headers = "From: Proyecto Corona <proyectocorona@outlook.es>\r\n";
+    $headers .= "Reply-to: proyectocorona@outlook.es\r\n";
+    $headers .= "Content-type: text/html\r\n";
+
+    mail($to, $subject, $message, $headers);
+
+    header("Location: ../reset-password.php?reset=success");
+} else {
+    header("Location: ../index.php");
+}
---
+++ reset-password.php
@@ -0,0 +1,29 @@
+<?php
+include('header.php');
+?>
+<div class="text-right container marginBot social-button">
+    <a href="#" target="_blank"><img src="img/instagram.png" alt="instagram" width="25px" class="logosRedes"></a>
+    <a href="https://www.youtube.com/channel/UC1U7M-rvlfWJ6mZcGQ33Mbw/videos" target="_blank"><img src="img/youtube.png" alt="youtube" width="25px" class="logosRedes"></a>
+    <a href="https://twitter.com/opsoms" target="_blank"><img src="img/twitter.png" alt="twitter" width="25px" class="logosRedes"></a>
+</div>
+<div class="inicio_sesion">
+    <h1 class="display-4 bordeBot">Renueva tu contraseña</h1>
+    <div class="texto centrar">
+        <p>Un e-mail le será enviado con las instrucciones para saber como renovar su contraseña.</p>
+        <form action="includes/reset-request.inc.php" method="post">
+            <input type="text" name="email" placeholder="Correo electrónico">
+            <button type="submit" name="reset-request-submit">Enviar</button>
+        </form>
+        <?php
+        if (isset($_GET["reset"])){
+            if ($_GET["reset"] == "success"){
+                echo '<p class="signupsuccess">Revisa tu e-mail!</p>';
+            }
+        }
+        ?>
+    </div>
+</div>
+
+<?php
+include('footer.php');
+?>
