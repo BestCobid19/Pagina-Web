18f36f143de83615376c06201372416be73da977
AitorPujadas <apujadasm@esliceu.net>
2020-05-08 12:07:18 +0200

    Update reset-password.inc.php

--- includes/reset-password.inc.php
+++ includes/reset-password.inc.php
@@ -25,7 +25,7 @@ if (isset($_POST["reset-password-submit"])){
         echo "Eso ha sido un error";
         exit();
     } else {
-         mysqli_stmt_bind_param($stmt, "s", $selector, $currentDate);
+         mysqli_stmt_bind_param($stmt, "ss", $selector, $currentDate);
          mysqli_stmt_execute($stmt);
 
          $result = mysqli_stmt_get_result($stmt);
