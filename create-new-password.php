c79c7fda6829cf46c3bc2a86c7bdb168bba80f8f
vzontor <63230088+vzontor@users.noreply.github.com>
2020-05-11 10:14:56 +0200

    dd

--- create-new-password.php
+++ create-new-password.php
@@ -6,7 +6,7 @@ include('header.php');
     <a href="https://www.youtube.com/channel/UC1U7M-rvlfWJ6mZcGQ33Mbw/videos" target="_blank"><img src="img/youtube.png" alt="youtube" width="25px" class="logosRedes"></a>
     <a href="https://twitter.com/opsoms" target="_blank"><img src="img/twitter.png" alt="twitter" width="25px" class="logosRedes"></a>
 </div>
-<div class="inicio_sesion">
+<div class="inicio_sesion, container">
     <div class="texto centrar">
         <?php
          $selector = $_GET["selector"];
@@ -22,7 +22,9 @@ include('header.php');
                 <form action="includes/reset-password.inc.php" method="post">
                    <input type="hidden" name="selector" value="<?php echo $selector ?>">
                    <input type="hidden" name="validator" value="<?php echo $validator ?>">
+                   <p>Nueca contraseña</p>
                    <input type="password" name="pwd" placeholder="Nueva Contraseña">
+                   <p>Repite la contraseña</p>
                    <input type="password" name="pwd-repeat" placeholder="Repita la Nueva Contraseña">
                    <button type="submit" name="reset-password-submit">Renovar</button>
                 </form>
