9627db6f106a510d1b84f6ba3a41117b8d1de67b
AitorPujadas <apujadasm@esliceu.net>
2020-05-14 12:00:24 +0200

    sdasd

--- header.php
+++ header.php
@@ -11,6 +11,7 @@ session_start();
     <script src="https://kit.fontawesome.com/1c44cf5aa1.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/testaitor.css">
     <link rel="shortcut icon" href="favicon.ico" />
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
 </head>
---
+++ sino.php
@@ -0,0 +1,48 @@
+<?php
+include('header.php');
+?>
+
+<div class="jumbotron jumbotron-fluid jumbotron1">
+    <div class="container">
+        <h1 class="display-4 bordeBot">Test Coronavirus</h1>
+
+
+    </div>
+</div>
+<div class="text-right container marginBot">
+    <a href="#" target="_blank"><img src="img/instagram.png" alt="instagram" width="25px" class="logosRedes"></a>
+    <a href="https://www.youtube.com/channel/UC1U7M-rvlfWJ6mZcGQ33Mbw/videos" target="_blank"><img src="img/youtube.png" alt="youtube" width="25px" class="logosRedes"></a>
+    <a href="https://twitter.com/opsoms" target="_blank"><img src="img/twitter.png" alt="twitter" width="25px" class="logosRedes"></a>
+</div>
+<div class="container borde">
+    <div class="texto">
+
+        <h2 class="texto"><span class="bordeIzquierdaFino"></span>No te preocupes, pareces tener los síntomas propios del coronavirus (SARS-CoV-2) y has estado expuesto en situaciones de riesgo, pero aún asi eso no quiere decir que estés contagiado.</h2>
+        <p>
+            Has contestado que:
+        </p>
+        <div class="marginIzquierda">
+            <ul class="col-6">
+                <li><b>SI </b>tienes todos los síntomas descritos.</li>
+                <li><b>NO </b>has estado en contacto con ningún caso sospechoso o confirmado o viajado a algunos países potencialmente peligrosos por coronavirus.</li>
+            </ul>
+        </div>
+        <p>
+        Es conveniente que te no te desplaces, llama al 112 o a cualquiera de estos teléfonos que aparecen en esta lista para comprobar tu estado y si es el caso, te darán instrucciones más precisas.
+        </p>
+        <p>
+            Para evitar posibles contagios a otras personas es conveniente que sigas estos consejos de prevención:
+        </p>
+        <div class="marginIzquierda">
+            <ul class="col-6">
+                <li>Lávate las manos a menudo (con agua y jabón o soluciones alcohólicas al 70%), especialmente después del contacto directo con otras personas.</li>
+                <li>Tápate la boca y la nariz con un pañuelo de un solo uso o con la parte del codo del brazo cuando no dispongas de ellos en el momento de estornudar o toser. Lávate las manos en seguida (con soluciones alcohólicas al 70% o con agua y jabón).</li>
+                <li>Evita el contacto directo (almenos una distancia de dos metros) con personas que padezcan infecciones respiratorias agudas y presenten tos, estornudos o expectoraciones, no compartas pertenencias personales.</li>
+                <li>Evita compartir comida y otros utensilios (vajilla, pañuelos, etc.) y otros objetos que no estén correctamente desinfectados.</li>
+            </ul>
+        </div>
+    </div>
+</div>
+<?php
+include('footer.php');
+?>
